8c3f11645137ed614469e6131fb32d39c26995d3
danielo <danielo@Mac-mini-de-daniel.local>
2025-11-03 12:29:53 -0600

    implementacion de inertia

--- app/Http/Controllers/State/StateController.php
+++ app/Http/Controllers/State/StateController.php
@@ -12,7 +12,8 @@ class StateController extends Controller
      */
     public function index()
     {
-        //
+        return inertia('StateIndex');
+
     }
 
     /**
---
+++ resources/js/Pages/StateIndex.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hello Inertia!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        // puedes recibir props de Laravel
+    },
+};
+</script>
